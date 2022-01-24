b57c707835c43f7d4fb4398b31871ff4d9e497f7
pradeeprjth <pradeep.cotocus@gmail.com>
2022-01-25 02:07:18 +0530

    Migrated data to npoint.io and use network call

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,16 @@
-// ignore_for_file: avoid_print, unnecessary_null_comparison, prefer_const_constructors, use_key_in_widget_constructors, deprecated_member_use
+// ignore_for_file: avoid_print, unnecessary_null_comparison, prefer_const_constructors, use_key_in_widget_constructors, deprecated_member_use, prefer_const_literals_to_create_immutables
 
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:fluttercatelog/core/store.dart';
+import 'package:fluttercatelog/models/cart.dart';
 import 'package:fluttercatelog/models/catalog.dart';
 import 'package:fluttercatelog/utils/routes.dart';
 import 'package:fluttercatelog/widgets/home_widgets/catalog_header.dart';
 import 'package:fluttercatelog/widgets/home_widgets/catalog_list.dart';
 import "package:velocity_x/velocity_x.dart";
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,6 +18,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final url = "https://api.npoint.io/c31db3b6185c948c3f8f";
   @override
   void initState() {
     super.initState();
@@ -24,8 +27,11 @@ class _HomePageState extends State<HomePage> {
 
   loadData() async {
     // await Future.delayed(Duration(seconds: 2));
-    final catalogJson =
-        await rootBundle.loadString("assets/files/catalog.json");
+    // final catalogJson =
+    //     await rootBundle.loadString("assets/files/catalog.json");
+    final response = await http.get(Uri.parse(url));
+    final catalogJson = response.body;
+
     final decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
     CatalogModel.items = List.from(productsData)
@@ -36,14 +42,26 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
         backgroundColor: context.canvasColor,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamed(context, MyRoutes.cartRoute);
+        floatingActionButton: VxBuilder(
+          mutations: {AddMutation, RemoveMutation},
+          builder: (context, store, status) {
+            return FloatingActionButton(
+              onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+              backgroundColor: context.theme.buttonColor,
+              child: Icon(
+                CupertinoIcons.cart,
+                color: Colors.white,
+              ),
+            ).badge(
+                color: Vx.green500,
+                size: 22,
+                count: _cart.items.length,
+                textStyle: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold));
           },
-          backgroundColor: context.theme.buttonColor,
-          child: Icon(CupertinoIcons.cart, color: Colors.white),
         ),
         body: SafeArea(
           child: Container(
